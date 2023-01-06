57a79700f5b113c4b6297d6b113e3fb8b080a56a
Evan <118688666+Evian04@users.noreply.github.com>
2023-01-06 13:13:26 +0100

    finished program for a human player

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from src.deminer import Deminer
 
 
-game = Deminer(())
+game = Deminer((10, 10), 20)
+game.display_grid()
+
+while True:
+    action = input("Do you wanna explore, report or unreport a place : ")
+
+    match action:
+        case "explore":
+            game.explore(int(input("i : ")), int(input("j : ")))
+        
+        case "report":
+            game.report_mine(int(input("i : ")), int(input("j : ")))
+        
+        case "unreport":
+            game.cancel_report(int(input("i : ")), int(input("j : ")))
+        
+        case other:
+            print(f"{action} doesn't march with any keyword")
+        
+    game.display_grid()
--- src/deminer.py
+++ src/deminer.py
@@ -7,8 +7,20 @@ class Deminer:
         self.dimensions = dimensions
         self.mines_grid = self.generate_mines_grid(mine_percentage)
         self.exploration_grid = self.generate_exploration_grid()
-
         self.is_explosed = False
+
+        self.first_exploration()
+    
+    def first_exploration(self):
+        empty_places = []
+        
+        for i in range(self.dimensions[0]):
+            for j in range(self.dimensions[1]):
+                if not self.mines_grid[i][j] and self.get_near_mines(i, j) == 0:
+                    empty_places.append((i, j))
+
+        chosen_place = random.choice(empty_places)
+        self.explore(chosen_place[0], chosen_place[1])
 
     def generate_mines_grid(self, percentage: int) -> list[list[int]]:
         grid = []
@@ -31,16 +43,24 @@ class Deminer:
     def generate_exploration_grid(self) -> list[list[str]]:
         return [["nothing" for j in range(self.dimensions[1])] for i in range(self.dimensions[0])]
     
-    def explore_place(self, i: int, j: int):
+    def explore(self, i: int, j: int):
         if self.exploration_grid[i][j] == "reported":
-            print("This place is reported, please unreport it before explore it")
+            print("This place is reported, if you want to explore it you must unreport it before")
             return
 
         if self.mines_grid[i][j]:
             self.is_explosed = True
+            print("Explosion !!")
             return
         
         self.exploration_grid[i][j] = "explored"
+
+        if self.get_near_mines(i, j) == 0:
+            for add_i in range(-1, 2):
+                for add_j in range(-1, 2):
+                    if i + add_i in range(self.dimensions[0]) and j + add_j in range(self.dimensions[1]):
+                        if self.exploration_grid[i + add_i][j + add_j] != "explored":
+                            self.explore(i + add_i, j + add_j)
     
     def report_mine(self, i: int, j: int):
         self.exploration_grid[i][j] = "reported"
@@ -66,9 +86,10 @@ class Deminer:
         return sum
     
     def display_grid(self):
-        print("=" * self.dimensions[1] + "\n" * 2)
+        print("   " + " ".join([str(j) for j in range(self.dimensions[1])]) + "\n   " + "=" * self.dimensions[1] * 2)
+
         for i in range(self.dimensions[0]):
-            line = ""
+            line = f"{i} |"
             for j in range(self.dimensions[1]):
 
                 match self.exploration_grid[i][j]:
@@ -83,4 +104,6 @@ class Deminer:
 
                 line += " "
             
-            print(line)
+            print(line + "|")
+        
+        print("   " + "=" * self.dimensions[1] * 2)
