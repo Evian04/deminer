7706f37f856aaa2ed0fcfe6952ff32f382afdea9
Evan <118688666+Evian04@users.noreply.github.com>
2023-01-10 10:31:09 +0100

    adding solver's class skeleton

---
+++ src/solver.py
@@ -0,0 +1,12 @@
+import random
+
+from deminer import Deminer
+
+
+class Solver:
+    
+    def __init__(self, deminer: Deminer):
+        self.game = deminer
+    
+    def get_probability(self):
+        pass
